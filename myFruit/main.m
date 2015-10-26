83dbf632da21f88829ffb7591d0f7f99e1925964
runningcoco <627010288@qq.com>
2015-10-26 22:46:48 +0800

    Initial Commit

---
+++ myFruit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  myFruit
+//
+//  Created by 我是僵尸 on 15/10/26.
+//  Copyright (c) 2015年 echochief. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
